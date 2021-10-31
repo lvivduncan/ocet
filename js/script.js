a8f4050b62ded0869e4ada7cd1e4c3b4e2c27734
lvivduncan <lvivduncan@gmail.com>
2021-10-31 20:36:31 +0200

    basket

--- js/script.js
+++ js/script.js
@@ -43,3 +43,112 @@ $('#menu-button').on('click', function(){
     $('#menu-button').toggleClass('white');
     $('body').toggleClass('fixed');
 });
+
+
+
+
+
+
+
+// замовлення 
+let localContent;
+let localPrice;
+
+upgradeCheckout();
+
+
+
+// клік на кнопку замовлення
+$('.dish button').on('click', function(){
+
+    // перевіряємо чи є дані у базі
+    if(localStorage.getItem('order-content') !== null){
+
+        // отримуємо дані з бази
+        localContent = localStorage.getItem('order-content');
+        localPrice = localStorage.getItem('order-price');
+    }
+
+
+    // назва страви 
+    let content = $(this).data('content');
+
+    // вартість
+    let price = $(this).data('price');
+
+    // якщо прийшли дані з бази
+    if(localContent !== undefined){
+
+        localContent += ';' + content;
+        localPrice += ';' + price;
+
+    } else {
+
+        localContent = content;
+        localPrice = price;
+    }
+
+    // надсилаємо дані в базу після обробки
+    localStorage.setItem('order-content', localContent);
+    localStorage.setItem('order-price', localPrice);
+
+    // оновлюємо дані кошика
+    upgradeCheckout();
+});
+
+$('#checkout-goods').on('click', function(event){
+
+    // номер товару у кошикові
+    const id = event.target.dataset.id;
+
+    let localContent1 = localStorage.getItem('order-content').split(';');
+    let localPrice1 = localStorage.getItem('order-price').split(';');
+
+    localContent1.splice(id, 1);
+    localPrice1.splice(id, 1);
+
+    // надсилаємо дані в базу після обробки
+    localStorage.setItem('order-content', localContent1.join(';'));
+    localStorage.setItem('order-price', localPrice1.join(';'));
+
+    // оновлюємо дані кошика
+    upgradeCheckout();
+});
+
+// оновлення даних кошика
+function upgradeCheckout(){
+
+    // смикаємо базу, обробляємо дані і виводимо
+    if(localStorage.getItem('order-content') !== null){
+
+        // показати кошик, якщо є дані
+        $('#checkout').addClass('active');
+
+        let localContent = localStorage.getItem('order-content').split(';');
+        let localPrice = localStorage.getItem('order-price').split(';');
+    
+        let content = '';
+        let price = 0;
+
+        for(let i = 0; i<localContent.length; i++){
+
+            content += '<li data-id="' + i + '">' + i + ' - ' + localContent[i] + '</li>';
+            price += +localPrice[i];
+        }
+
+        // додаємо дані
+        $('#checkout-goods').html(content);
+
+        $('#checkout-price strong').html(price);
+    } else {
+
+        // ховаємо кошик, якщо даних немає 
+        $('#checkout').removeClass('active');
+    }
+}
+
+// видаляємо дані з кошика (по 1 ітему)
+function deleteGoods(){
+
+
+}
