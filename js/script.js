1b0eb803fb18c902f2908b76c3d81c81bcdd74df
lvivduncan <lvivduncan@gmail.com>
2021-11-02 21:38:28 +0200

    повернув код відправки форми бронювання

--- js/script.js
+++ js/script.js
@@ -41,7 +41,33 @@ $('#menu-button').on('click', function(){
     $('body').toggleClass('fixed');
 });
 
+// бронювання столика
+$('#form').trigger('reset');
 
+$(function () {
+    'use strict';
+    $('#form').on('submit', function (e) {
+        e.preventDefault();
+            $.ajax({
+                url: '../mail/send.php',
+                type: 'POST',
+                contentType: false,
+                processData: false,
+                data: new FormData(this),
+                success: function (msg) {
+                console.log(msg);
+                if (msg == 'ok') {
+                    setTimeout(() => {$.fancybox.open(`<p>Лист надіслано!</p>`), 1000});
+                    setTimeout(() => $.fancybox.close(), 2000);
+                    $('#form').trigger('reset'); // очистка формы
+                } else {
+                    setTimeout(() => {$.fancybox.open(`<p>Не вдалося надіслати лист!</p>`), 1000});
+                    setTimeout(() => $.fancybox.close(), 2000);
+                }
+            }
+        });
+    });
+});
 
 
 
